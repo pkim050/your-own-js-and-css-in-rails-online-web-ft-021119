ca81b204fa35c8f21ed5faa89b61d1f174c13f45
Patrick Kim <pkim050@gmail.com>
2019-07-03 18:22:06 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   gem 'byebug'
   gem 'rspec-rails', '~> 3.0'
   gem 'capybara'
+  gem 'puma'
   gem 'selenium-webdriver'
   gem 'poltergeist'
 end
---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  const body = document.querySelector("body")
+  body.addEventListener("click", function(e) {
+      $("#hide_this").html("")
+    })
+})
