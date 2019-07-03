00f562830dd3ccd439c996cb65d706f1fc1ac5ec
Patrick Kim <pkim050@gmail.com>
2019-07-03 18:15:53 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
   const body = document.querySelector("body")
   body.addEventListener("click", function(e) {
-    debugger
-    if (e.target.id === "hide_this") {
       $("#hide_this").html("")
     }
-  })
 })
